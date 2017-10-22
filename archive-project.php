4e445bd5aa326802bb379b64eef24ae3e7593bbd
Olivia Orr <oorr90@gmail.com>
2017-10-22 17:00:01 -0400

    Getting started on the archive project page

---
+++ archive-project.php
@@ -0,0 +1,14 @@
+<?php echo get_header(); ?>
+
+
+    <!-- MAIN -->
+
+    <main>
+    
+        <div id="inner-page" class="all-projects-main">
+        
+        </div>
+
+    </main>
+    
+<?php echo get_footer(); ?>
