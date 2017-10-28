74e4639901be3c0b06b257f92c2b0fb39b6b56af
Olivia Orr <oorr90@gmail.com>
2017-10-28 19:54:18 -0400

    Adding the All Projects page

--- archive-project.php
+++ archive-project.php
@@ -5,8 +5,86 @@
 
     <main>
     
-        <div id="inner-page" class="all-projects-main">
-        
+        <div id="inner-page">
+            <div id="inner-head">
+                <h1>Artist Collections</h1>
+            </div>
+            
+            <div id="main-projects">
+                <a href="#" class="single-project-container"><div>
+                    <img img src="img/img_placeholder.jpg" alt="Project image" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+                <a href="#" class="single-project-container"><div>
+                    <img img src="../Assets/sample_home_gallery3.png" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+                <a href="#" class="single-project-container"><div>
+                    <img img src="../Assets/sample_home_gallery2.png" alt="Project image" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+                <a href="#" class="single-project-container"><div>
+                    <img img src="../Assets/sample_home_gallery1.JPG" alt="Project image" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+                
+               <a href="#" class="single-project-container"><div>
+                    <img img src="../Assets/sample_project_gallery4.jpeg" alt="Project image" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+                <a href="#" class="single-project-container"><div>
+                    <img img src="../Assets/sample_project_gallery1.jpeg" alt="Project image" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+                <a href="#" class="single-project-container"><div>
+                    <img img src="../Assets/sample_project_gallery2.jpeg" alt="Project image" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+                <a href="#" class="single-project-container"><div>
+                    <img img src="../Assets/sample_project_gallery3.jpeg" alt="Project image" class="image">
+                    <div class="overlay">
+                        <div class="overlay-text">
+                            <div class="overlay-text-artist">John Doe</div>
+                            <div class="overlay-text-project">Name of Collection</div>
+                        </div>
+                    </div>
+                </div></a>
+            </div>
         </div>
 
     </main>
