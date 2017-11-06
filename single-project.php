3e94b99574567d9f08c9d300001f2c61da6f91ab
Olivia Orr <oorr90@gmail.com>
2017-11-06 16:26:54 -0500

    Editing the styling on the single project display page

--- single-project.php
+++ single-project.php
@@ -23,52 +23,43 @@
 <main>
 
     <div id="inner-page" class="single-project-main">
-        <div class="single-project-head">
-            <div class="single-project-artist">
-                <h2><span class="single-project-artist-first"><?php echo $artist -> additional_fields['first_name']; ?></span>
-                
-                  <span class="single-project-artist-last"><?php echo $artist -> additional_fields['last_name']; ?></span></h2>
-            </div>
-            <div class="single-project-title">
-                <h1><?php echo $project -> post_title; ?></h1>
-            </div>
-        </div>
-        <div class="single-project-description">
-            <p><?php echo $project -> post_content; ?></p>
-        </div>
-        <div class="single-project-section">
-           <!-- TWO COLUMNS IF IMAGES (is this possible?) -->
-           <div class="project-with-thumbnails">
-               <p>Click thumbnails to enlarge</p>
-                <div class="thumbnails-display">
-                   
-                   <?php 
-                    
-                        foreach ( $project -> additional_fields['images'] as $img ) {
-                            echo ' <div class="one-thumbnail">
-                        <a href="#"><img src="' . $img['image']['sizes']['thumbnail'] . '" alt="'  . $img['image']['alt'] .'"></a>
-                    </div> '; 
-                        }
-                    
-                    ?>
+       
+       <div class="project-sidebar">
+            <div class="single-project-head">
+                <div class="single-project-artist">
+                    <h3><span class="single-project-artist-first"><?php echo $artist -> additional_fields['first_name']; ?></span>
 
+                      <span class="single-project-artist-last"><?php echo $artist -> additional_fields['last_name']; ?></span></h3>
+                </div>
+                <div class="single-project-title">
+                    <h2><?php echo $project -> post_title; ?></h2>
                 </div>
             </div>
-            <div class="single-project-image-lg">
-               <!-- Max mentioned that he wanted people to be able to click through photos. My idea was that you can click on any of the small thumbnails, which will then pop up as the main image here...not sure if that is possible! -->
-                <img src="img/img_placeholder2.jpeg" alt="image title">
+
+            <div class="single-project-description">
+                <p><?php echo $project -> post_content; ?></p>
             </div>
-           
-            <!-- ONE COLUMN IF VIDEO (is this possible?) -->
-            <!-- <div class="project-with-video">
-                <iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k">
-                </iframe>
-            </div> -->
+            
+            <h4>Click Thumbnails to enlarge &rarr;</h4>
         </div>
         
-        <!--<div class="project-comments">
-            Not sure if Max still wants this...but I am assuming there is a plugin for comments, and that I do not need to hand code it!
-        </div>-->
+        <div class="project-content-container">
+        
+        <?php
+        
+        if ($project -> additional_fields['type'] == "Photography") {
+                       
+                    foreach ( $project -> additional_fields['images'] as $img ) {
+                        echo    '<div class="one-thumbnail">
+                                <img src="' . $img['image']['sizes']['medium'] . '" alt="'  . $img['image']['alt'] .'">
+                                </div>'; 
+                    }
+        }
+        
+        ?>
+        
+        <!-- end project-content-container -->
+        </div>
         
         <div class="project-browse-options">
             <div class="back-button">
@@ -81,6 +72,7 @@
                 <a href="<?php echo site_url(); ?>/artist/">Back to all artists &rarr;</a>
             </div>
         </div>
+        
     <!-- End inner page -->
     </div>
 	
