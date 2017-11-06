a69be51d53ed61bc55a078cc72abffe73bd1642e
Olivia Orr <oorr90@gmail.com>
2017-11-06 17:13:56 -0500

    Getting rid of unnecessary markup

--- single-project.php
+++ single-project.php
@@ -22,58 +22,49 @@
 <!-- MAIN --> 
 <main>
 
-    <div id="inner-page" class="single-project-main">
+    <div id="inner-page">
        
        <div class="project-sidebar">
-            <div class="single-project-head">
-                <div class="single-project-artist">
-                    <h3><span class="single-project-artist-first"><?php echo $artist -> additional_fields['first_name']; ?></span>
-
-                      <span class="single-project-artist-last"><?php echo $artist -> additional_fields['last_name']; ?></span></h3>
-                </div>
-                <div class="single-project-title">
-                    <h2><?php echo $project -> post_title; ?></h2>
-                </div>
-            </div>
-
-            <div class="single-project-description">
-                <p><?php echo $project -> post_content; ?></p>
-            </div>
+            <h3><?php echo $artist -> additional_fields['first_name']; ?> <?php echo $artist -> additional_fields['last_name']; ?></h3>
             
-            <h4>Click Thumbnails to enlarge &rarr;</h4>
+            <h2><?php echo $project -> post_title; ?></h2>
+
+            <!-- PROJECT DESCRIPTION -->
+            <p><?php echo $project -> post_content; ?></p>
+            
+            <h4><a href="<?php echo $artist -> permalink; ?>">View Artist Bio</a></h4>
         </div>
         
         <div class="project-content-container">
         
-        <?php
+            <?php
+
+            if ($project -> additional_fields['type'] == "Photography") {
+
+                foreach ( $project -> additional_fields['images'] as $img ) {
+                    echo    '<div class="one-thumbnail">
+                            <a href="#"><img src="' . $img['image']['sizes']['medium'] . '" alt="'  . $img['image']['alt'] .'"></a>
+                            </div>'; 
+                }
+            } else if ($project -> additional_fields['type'] == "Video") {
+                echo "video goes here"; 
+            }
+
+            ?>
         
-        if ($project -> additional_fields['type'] == "Photography") {
-                       
-                    foreach ( $project -> additional_fields['images'] as $img ) {
-                        echo    '<div class="one-thumbnail">
-                                <img src="' . $img['image']['sizes']['medium'] . '" alt="'  . $img['image']['alt'] .'">
-                                </div>'; 
-                    }
-        }
-        
-        ?>
-        
-        <!-- end project-content-container -->
+        <!-- END project-content-container -->
         </div>
         
         <div class="project-browse-options">
             <div class="back-button">
-                <a href="<?php echo site_url(); ?>/project/">&larr; Back to all projects</a>
+                <a href="<?php echo site_url(); ?>/project/">&larr; all projects</a>
             </div>
             <div class="back-button">
-                <a href="<?php echo $artist -> permalink; ?>">View Artist Bio</a>
-            </div>
-            <div class="back-button">
-                <a href="<?php echo site_url(); ?>/artist/">Back to all artists &rarr;</a>
+                <a href="<?php echo site_url(); ?>/artist/">all artists &rarr;</a>
             </div>
         </div>
         
-    <!-- End inner page -->
+    <!-- END inner-page -->
     </div>
 	
  </main>
