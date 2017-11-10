78800977611587e9dcc7e565708e4f7311198a46
Olivia Orr <oorr90@gmail.com>
2017-11-10 10:32:25 -0500

    Styling the home page

--- page-front.php
+++ page-front.php
@@ -6,40 +6,54 @@
 
 
 <!-- MAIN -->
-<main>
-   <div id="home-feature" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/example/sample_stock4.jpeg');">
-        <div id="home-feature-artist">
-            <p id="artist-name">John Doe</p>
-            <p id="artist-word">"Hipster Shit" photo series</p>
+<main class="home-wrap">
+   
+    <div class="home-featured" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/example/sample_stock4.jpeg');">
+        <div class="home-button feature-button"><a href="#">
+            <p>From "Title of Collection"</p>
+            <p>John Doe</p>
+        </a></div>   
+    </div>
+    
+    <div class="tagline">
+        <h1>Innovate<br><span>&amp;</span><br>Collaborate</h1>
+    </div>
+    
+    <div class="home-intro" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/example/sample_stock4.jpeg');">
+        <div class="home-intro-text">
+            <p class="intro-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam accumsan nulla tellus, vel lacinia neque condimentum ut. Integer malesuada ornare augue, tempor efficitur est consectetur id. Sed egestas, enim ac tempor tempor, neque lorem cursus massa, non tincidunt arcu sapien vel neque.</p>
+            <div class="home-button">
+                <p><a href="#">Our Mission</a></p>
+            </div>
         </div>
     </div>
+    
+    <div class="home-featured-secondary">
+        <div class="latest-works">
+            <div class="line"></div>
+            <div>
+                <h2>Latest Works</h2>
+            </div>
+            <div class="line"></div>
+        </div>
 
-    <div id="tagline-main">
-        <div class="tagline">
-            <h1>Innovate &amp; Collaborate</h1>
+        <div class="featured-works">
+            <div class="home-featured-item">
+                <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/example/sample_home_gallery1.JPG" alt="money and sex"></a>
+            </div>
+            <div class="home-featured-item">
+                <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/example/sample_home_gallery2.png" alt="collage and birds"></a>
+            </div>
+            <div class="home-featured-item">
+                <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/example/sample_home_gallery3.png" alt="a women illustration"></a>
+            </div>
+        </div>
+        
+        <div class="home-button">
+            <p><a href="#">Browse All</a></p>
         </div>
     </div>
-
-    <div id="view-more-main">Click to View<br>Featured Portfolios</div>
-    <div id="arrow">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/down_arrow.png" alt="down arrow">
-    </div>
-
-    <div id="featured-works">
-        <div class="home-featured-item">
-            <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/example/sample_home_gallery1.JPG" alt="money and sex"></a>
-        </div>
-        <div class="home-featured-item">
-            <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/example/sample_home_gallery2.png" alt="collage and birds"></a>
-        </div>
-        <div class="home-featured-item">
-            <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/example/sample_home_gallery3.png" alt="a women illustration"></a>
-        </div>
-    </div>
-
-    <div id="portfolio">
-        <h2><a href="#">Browse All Portfolios</a></h2>
-    </div>
+    
 </main>
 
 
