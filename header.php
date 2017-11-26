40cec17cc9d89390f7dcb85902cfa0d180785440
Olivia Orr <oorr90@gmail.com>
2017-11-26 12:32:32 -0500

    Pointing the logo to the home page

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 				<!-- HEADER -->
 		    <header>
 		        <div class="header-logo">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/beacon_207_test.png" alt="Beacon Media Lighthouse Logo">
+                    <a href="<?php echo home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/beacon_207_test.png" alt="Beacon Media Lighthouse Logo"></a>
 		        </div>
 
         	  	<nav role="navigation">
