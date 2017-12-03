6acdb6e50f9f6d74b38dbcd45fdc03650dbd1be5
Olivia Orr <oorr90@gmail.com>
2017-12-03 13:37:15 -0500

    Adding favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
 
+        <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico" />
 
 		<!-- LOAD CSS -->
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/normalize.css">
